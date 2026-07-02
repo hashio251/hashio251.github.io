5d977bd5a84fd60a14fc2ccd69419f8f3f11b722
Yuka Hashio <251zs03753@student.zen.ac.jp>
2026-07-02 15:13:18 +0900

    change garally slider to scroll

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,7 +1,21 @@
 $('.autoplay').slick({
-  slidesToShow: 1,
+  slidesToShow: 4,
   slidesToScroll: 1,
   autoplay: true,
   autoplaySpeed: 1800,
-  dots: true
+  dots: true,
+
+  // responsive
+  responsive: [
+    {
+      breakpoint: 1500,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 
+    }
+  ]
 });
